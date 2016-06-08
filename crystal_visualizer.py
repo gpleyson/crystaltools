15ee2ff20c87c12297e4fb59c8f34e1ac4d4ee5a
Gerard Paul Marcelo Leyson <dagatkarimlan@gmail.com>
2016-06-08 18:25:58 +0200

    created crystal gene and vis examples
    Created sample Jupyter notebooks for crystal generation and
    crystal visualization. Also linted crystal_visualizer.

--- crystal_visualizer.py
+++ crystal_visualizer.py
@@ -1,6 +1,3 @@
-# import os
-# import sys
-# import time
 import copy
 import numpy as np
 # import scipy as sp
@@ -55,8 +52,8 @@ class CrystalPlot2D():
         fig = plt.figure(fignum, figsize=(self.fwidth, self.fheight))
         fig.clf()
         ax = fig.add_subplot(111)
-        hf2 = self._plot_crystal_cell(crys, ax)
-        hf1 = self._scatter_basis(crys, ax, zorder=5)
+        self._plot_crystal_cell(crys, ax)
+        self._scatter_basis(crys, ax, zorder=5)
 
         plt.axis('equal')
 
@@ -75,8 +72,6 @@ class CrystalPlot2D():
             hfig = ax.scatter(x, y, marker=self.marker, s=self.ms,
                               facecolors=self.mfc, zorder=z)
 
-            print z
-
         return hfig
 
     def _plot_crystal_cell(self, crys, ax):
@@ -85,22 +80,18 @@ class CrystalPlot2D():
         p2 = crys.lattvec[1, 0:2] * crys.scale
         p3 = crys.lattvec[2, 0:2] * crys.scale
 
-        hfig = self._plot_line(ax, p0      , p1      , zorder=-100)
-        hfig = self._plot_line(ax, p1      , p1+p2   , zorder=-100)
-        hfig = self._plot_line(ax, p1+p2   , p2      , zorder=-100)
-        hfig = self._plot_line(ax, p2      , p0      , zorder=-100)
-        hfig = self._plot_line(ax, p0   +p3, p1   +p3, zorder=-100)
-        hfig = self._plot_line(ax, p1   +p3, p1+p2+p3, zorder=-100)
-        hfig = self._plot_line(ax, p1+p2+p3, p2   +p3, zorder=-100)
-        hfig = self._plot_line(ax, p2   +p3, p0   +p3, zorder=-100)
-        hfig = self._plot_line(ax, p0      , p0   +p3, zorder=-100)
-        hfig = self._plot_line(ax, p1      , p1   +p3, zorder=-100)
-        hfig = self._plot_line(ax, p1+p2   , p1+p2+p3, zorder=-100)
-        hfig = self._plot_line(ax, p2      , p2   +p3, zorder=-100)
-
-        # hfig = self._plot_line(ax, p0, p2, zorder=3)
-        # hfig = self._plot_line(ax, p0, p3, zorder=3)
-        # hfig = self._plot_line(ax, p0, p3, zorder=3)
+        self._plot_line(ax, p0, p1, zorder=-100)
+        self._plot_line(ax, p1, p1+p2, zorder=-100)
+        self._plot_line(ax, p1+p2, p2, zorder=-100)
+        self._plot_line(ax, p2, p0, zorder=-100)
+        self._plot_line(ax, p0+p3, p1+p3, zorder=-100)
+        self._plot_line(ax, p1+p3, p1+p2+p3, zorder=-100)
+        self._plot_line(ax, p1+p2+p3, p2+p3, zorder=-100)
+        self._plot_line(ax, p2+p3, p0+p3, zorder=-100)
+        self._plot_line(ax, p0, p0+p3, zorder=-100)
+        self._plot_line(ax, p1, p1+p3, zorder=-100)
+        self._plot_line(ax, p1+p2, p1+p2+p3, zorder=-100)
+        self._plot_line(ax, p2, p2+p3, zorder=-100)
 
     def _plot_line(self, ax, p1, p2, zorder=1):
         """ Plots a line from p1 to p2 """
@@ -109,9 +100,10 @@ class CrystalPlot2D():
 
         return hfig
 
+
 class CrystalPlot3D():
-    def __init__(self, ms=40., lc='k', lw=1.5, mfc='r', marker='o', \
-            fwidth = 4., fheight=4., bool_interactive=True):
+    def __init__(self, ms=40., lc='k', lw=1.5, mfc='r', marker='o',
+                 fwidth=4., fheight=4., bool_interactive=True):
         self.ms = ms
         self.marker = marker
         self.lc = lc
@@ -125,7 +117,7 @@ class CrystalPlot3D():
         plt.rc('text', usetex=True)
         plt.rc('font', family='serif')
 
-    def plot_simple(self, crystal, v1=[], v2=[], fignum=1 ):
+    def plot_simple(self, crystal, v1=[], v2=[], fignum=1):
         """
             Simple 2D plot of the crystal
             crys - crsytal
@@ -135,13 +127,13 @@ class CrystalPlot3D():
         crys = copy.deepcopy(crystal)
 
         # set default values for v1 and v2 if not defined
-        if len(v1)==0:
-            v1 = crys.lattvec()[0,:]
-        if len(v2)==0:
-            v2 = crys.lattvec()[1,:]
+        if len(v1) == 0:
+            v1 = crys.lattvec()[0, :]
+        if len(v2) == 0:
+            v2 = crys.lattvec()[1, :]
 
         # calculate rotation matrix
-        Rmat = ct.create_rotation_matrix(v1,v2)
+        Rmat = ct.create_rotation_matrix(v1, v2)
 
         # rotate crystal
         crys.rotate_crystal(Rmat)
@@ -153,8 +145,8 @@ class CrystalPlot3D():
         fig = plt.figure(fignum, figsize=(self.fwidth, self.fheight))
         fig.clf()
         ax = fig.add_subplot(111, projection='3d')
-        hf2 = self._plot_crystal_cell(crys, ax)
-        hf1 = self._scatter_basis(crys, ax, zorder=5)
+        self._plot_crystal_cell(crys, ax)
+        self._scatter_basis(crys, ax, zorder=5)
 
         ax.set_aspect('equal')
         ax.set_xlabel('x')
@@ -173,8 +165,8 @@ class CrystalPlot3D():
                           facecolors=self.mfc, zorder=zorder)
 
         max_range = np.array(
-                [xcar.max()-xcar.min(), ycar.max()-ycar.min(),
-                 zcar.max()-zcar.min()]).max() / 2.0 * 1.5
+            [xcar.max()-xcar.min(), ycar.max()-ycar.min(),
+             zcar.max()-zcar.min()]).max() / 2.0 * 1.5
 
         mean_x = xcar.mean()
         mean_y = ycar.mean()
@@ -187,44 +179,40 @@ class CrystalPlot3D():
 
     def _plot_crystal_cell(self, crys, ax):
         p0 = np.zeros(3)
-        p1 = crys.lattvec[0,:] * crys.scale
-        p2 = crys.lattvec[1,:] * crys.scale
-        p3 = crys.lattvec[2,:] * crys.scale
+        p1 = crys.lattvec[0, :] * crys.scale
+        p2 = crys.lattvec[1, :] * crys.scale
+        p3 = crys.lattvec[2, :] * crys.scale
 
-        hfig = self._plot_line(ax, p0      , p1      , zorder=1)
-        hfig = self._plot_line(ax, p1      , p1+p2   , zorder=1)
-        hfig = self._plot_line(ax, p1+p2   , p2      , zorder=1)
-        hfig = self._plot_line(ax, p2      , p0      , zorder=1)
+        self._plot_line(ax, p0, p1, zorder=1)
+        self._plot_line(ax, p1, p1+p2, zorder=1)
+        self._plot_line(ax, p1+p2, p2, zorder=1)
+        self._plot_line(ax, p2, p0, zorder=1)
 
-        hfig = self._plot_line(ax, p0   +p3, p1   +p3, zorder=1)
-        hfig = self._plot_line(ax, p1   +p3, p1+p2+p3, zorder=1)
-        hfig = self._plot_line(ax, p1+p2+p3, p2   +p3, zorder=1)
-        hfig = self._plot_line(ax, p2   +p3, p0   +p3, zorder=1)
+        self._plot_line(ax, p0+p3, p1+p3, zorder=1)
+        self._plot_line(ax, p1+p3, p1+p2+p3, zorder=1)
+        self._plot_line(ax, p1+p2+p3, p2+p3, zorder=1)
+        self._plot_line(ax, p2+p3, p0+p3, zorder=1)
 
-        hfig = self._plot_line(ax, p0      , p0   +p3, zorder=1)
-        hfig = self._plot_line(ax, p1      , p1   +p3, zorder=1)
-        hfig = self._plot_line(ax, p1+p2   , p1+p2+p3, zorder=1)
-        hfig = self._plot_line(ax, p2      , p2   +p3, zorder=1)
-
-        #hfig = self._plot_line(ax, p0, p2, zorder=3)
-        #hfig = self._plot_line(ax, p0, p3, zorder=3)
-        #hfig = self._plot_line(ax, p0, p3, zorder=3)
-
+        self._plot_line(ax, p0, p0+p3, zorder=1)
+        self._plot_line(ax, p1, p1+p3, zorder=1)
+        self._plot_line(ax, p1+p2, p1+p2+p3, zorder=1)
+        self._plot_line(ax, p2, p2+p3, zorder=1)
 
     def _plot_line(self, ax, p1, p2, zorder=1):
         """ Plots a line from p1 to p2 """
-        hfig = ax.plot([p1[0], p2[0]], [p1[1], p2[1]], [p1[2], p2[2]],\
-                '-', lw=self.lw, color=self.lc)
+        hfig = ax.plot([p1[0], p2[0]], [p1[1], p2[1]], [p1[2], p2[2]],
+                       '-', lw=self.lw, color=self.lc)
 
         return hfig
+
 
 def orthogonal_proj(zfront, zback):
     a = (zfront+zback)/(zfront-zback)
     b = -2*(zfront*zback)/(zfront-zback)
-    return np.array([[1,0,0,0],
-                     [0,1,0,0],
-                     [0,0,a,b],
-                     [0,0,-1.e-10,zback]])
+    return np.array([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, a, b],
+                     [0, 0, -1.e-10, zback]])
 
 if __name__ == "__main__":
     proj3d.persp_transformation = orthogonal_proj
@@ -255,32 +243,33 @@ if __name__ == "__main__":
     myplot = CrystalPlot3D(lc='0.5', ms=80)
     crys, fig, ax = myplot.plot_simple(sc, v1, v2, fignum=2)
 
-    #for ii in range(1):
-    #    unit = cg.get_unit_cell('fcc primitive', 2.8)
+    # for ii in range(1):
+    #     unit = cg.get_unit_cell('fcc primitive', 2.8)
 
-    #    v1 = np.array([-1., 1., 0.])
-    #    v2 = np.array([ 1., 1., 1.])
+    #     v1 = np.array([-1., 1., 0.])
+    #     v2 = np.array([ 1., 1., 1.])
 
-    #    #v1 = np.array([-1., 1., 0.])
-    #    #v2 = np.array([ 0., 0., 1.])
+    #     #v1 = np.array([-1., 1., 0.])
+    #     #v2 = np.array([ 0., 0., 1.])
 
-    #    v3 = np.cross(v1,v2)
+    #     v3 = np.cross(v1,v2)
 
-    #    #v1 = np.array([ 1., 1., 0.])
-    #    #v2 = np.array([ 1., 0., 1.])
-    #    #v3 = np.array([ 0., 1., 1.])
+    #     #v1 = np.array([ 1., 1., 0.])
+    #     #v2 = np.array([ 1., 0., 1.])
+    #     #v3 = np.array([ 0., 1., 1.])
 
-    #    sc = unit.create_supercell(4,4,4, name='supercell')
-    #    newcell = unit.create_new_orientation(v1, v2, v3, name='new orientation')
-    #    newcell.reorient()
+    #     sc = unit.create_supercell(4,4,4, name='supercell')
+    #     newcell = unit.create_new_orientation(v1, v2, v3,
+    #                                           name='new orientation')
+    #     newcell.reorient()
 
-    #    dist = sc.find_distance_peroidic()
+    #     dist = sc.find_distance_peroidic()
 
-    #    a0 = sc.scale()
-    #    ep = a0 * 1.e-5
-    #    d0 = 0.
-    #    d1 = a0*(1./np.sqrt(2.))
-    #    d2 = a0
+    #     a0 = sc.scale()
+    #     ep = a0 * 1.e-5
+    #     d0 = 0.
+    #     d1 = a0*(1./np.sqrt(2.))
+    #     d2 = a0
 
-    #    idn1 = sc.get_neighbor_list(dmax=d1+ep, dmin=d0+ep)
-    #    idn2 = sc.get_neighbor_list(dmax=d2+ep, dmin=d1+ep)
+    #     idn1 = sc.get_neighbor_list(dmax=d1+ep, dmin=d0+ep)
+    #     idn2 = sc.get_neighbor_list(dmax=d2+ep, dmin=d1+ep)
